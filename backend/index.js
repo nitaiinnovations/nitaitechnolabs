b6c797ea7e82c515a841bc17dfea21df40538ba6
Ganesh <ganeshmk247@gmail.com>
2024-09-11 17:51:38 +0530

    hosting...

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: ["https://nitaiinnovations.netlify.app", process.env.FRONTEND_URL],
+  origin: ["https://nitaitechnolabs.netlify.app", process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
