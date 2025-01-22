b164921b8bc7dea4ed986bdfcbe1152b7699a914
nitaiinnovations <nvb@nitaiinnovations.com>
2025-01-22 14:47:17 +0530

    config

--- backend/routes/allBlogs.js
+++ backend/routes/allBlogs.js
@@ -3,7 +3,9 @@ const route = express.Router();
 const Blogs = require("../models/blogs");
 
 route.get("/", async (req, res) => {
+  console.log("came to all blogs")
   const blogs = await Blogs.find();
+  console.log("all blogs: ", blogs)
   res.send(blogs);
 });
 
